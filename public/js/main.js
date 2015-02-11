b0c37f14112e490d2cb368023ca7620fc68f3446
Austin Erlandson <austin@erlandson.com>
2015-02-11 10:04:58 -0600

    app is now functional. Posts to table and to database. Still need to fix testing.

--- public/js/main.js
+++ public/js/main.js
@@ -5,36 +5,47 @@ function hello() {
   return 'world';
 }
 
+var FIREBASE_URL = 'https://friends-address-book.firebaseio.com';
 
+$('#submit').click(submit);
 
-function addContactToTable (contact) {
-  
+function submit (event) {
+  event.preventDefault();
+  var contact = {
+    'photoUrl' : $('#photoUrl').val(),
+    'firstName' : $('#firstName').val(),
+    'lastName' : $('#lastName').val(),
+    'pNum1' : $('#pNum1').val(),
+    'pNum2' : $('#pNum2').val(),
+    'pNum3' : $('#pNum3').val(),
+    'emailAddress' : $('#emailAddress').val()
+  };
+  addContactToDb(contact, function (res) {
+    console.log(res);
+  });
+  addContactToTable(contact);
 }
 
+function addContactToDb(contact, cb) {
+  var url           = FIREBASE_URL + '/friends.json',
+      jsonData = JSON.stringify(contact);
+  $.post(url, jsonData, function (res) { return cb(res); });
+}
 
 function addContactToTable(contact) {
   var $row = $('<tr></tr>');
 
   if ($.isNumeric(contact.pNum1 || contact.pNum2 || contact.pNum3) === false) {
-    console.log('Please enter a valid phone number');
     return;
   }
 
   $row.append('<td>' + contact.photoURL + '</td>');
   $row.append('<td>' + contact.firstName + '</td>');
   $row.append('<td>' + contact.lastName + '</td>');
-  $row.append('<td>' + contact.pNum1 + '-' + contact.pNum2 + '-' + contact.pNum3 + '</td>');
+  $row.append('<td>' + contact.pNum1 + '-' + contact.pNum2  + '-' + contact.pNum3 + '</td>');
   $row.append('<td>' + contact.emailAddress + '</td>');
 
-  $('tbody').append($row);
+  $('#friendList').append($row);
 
   return $row;
 }
-
-
-
-
-
-
-
-
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -19,33 +19,12 @@ describe('DOM', function () {
   describe('table', function () {
     before(function () {
       if(window.__karma__) {
-        $('body').append('<table><thead></thead><tbody></tbody></table>');
+        $('body').append('<table><thead></thead><tbody id="friendList"></tbody></table>');
       }
     });
     beforeEach(function () {
       $('tbody').empty();
     });
-
-   // describe('refreshStockPrices', function () {
-   //   it('should edit each stock in the table with a new price', function () {
-   //     var stocks = [
-   //                    { Symbol: 'AAPL', LastPrice: 12.45 },
-   //                    { Symbol: 'MSFT', LastPrice: 23.56 }
-   //                  ],
-   //         $tbody = $('tbody'),
-   //         $trs;
-
-   //     $tbody.append('<tr><td>Apple Inc</td><td>AAPL</td><td>12.34</td><td>12.34</td></tr>');
-   //     $tbody.append('<tr><td>Microsoft Corp</td><td>MSFT</td><td>23.45</td><td>23.45</td></tr>');
-   //     $trs = $('tr');
-
-   //     refreshStockPrices(stocks);
-
-   //     $($($trs[0]).find('td')[3]).text().should.equal('12.45');
-   //     $($($trs[1]).find('td')[3]).text().should.equal('23.56');
-   //   });
-   // });
-
     describe('addContactToTable', function () {
       it('should add a row to the table', function () {
         var contact = {
@@ -85,9 +64,9 @@ describe('DOM', function () {
             pNum2       : '555',
             pNum3       : '5555',
             emailAddress: 'austin@erlandson.com'
-        },
-            $row        = addContactToTable(contact),
-            $tds        = $row.find('td');
+       },
+        $row = addContactToTable(contact),
+        $tds = $row.find('td');
 
         $tds.length.should.equal(5);
         $($tds[0]).text().should.equal('https://www.facebook.com/photo.php?fbid=10152885693074479&l=edfeb6ec76');
@@ -97,12 +76,31 @@ describe('DOM', function () {
         $($tds[4]).text().should.equal('austin@erlandson.com');
       });
     });
-    describe('sumbitButton', function () {
-      it('should post form data to database', function () {
-
+    describe('addContactToDb', function () {
+      it('should post form data to database', function (done) {
+        var contact     = {
+            photoURL    : 'https://www.facebook.com/photo.php?fbid=10152885693074479&l=edfeb6ec76',
+            firstName   : 'Austin',
+            lastName    : 'Erlandson',
+            pNum1       : '555',
+            pNum2       : '555',
+            pNum3       : '5555',
+            emailAddress: 'austin@erlandson.com'
+        };
+        addContactToDb(contact, function (res){
+          res.name.should.be.ok;
+          res.name.length.should.equal(20);
+          done();
+        });
       });
-      it('should add form data to table', function () {
-
+    });
+    describe('retrieveFriends', function () {
+      it('should clear the dom table', function () {
+        retrieveFriends();
+        $('tr').length.should.equal(0);
+      });
+      it('should repopulate the dom with data from firebase', function () {
+  //      retrieve
       });
     });
   });
